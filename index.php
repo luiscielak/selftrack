9f2271405b582ffa2a86f31ffc4da5472f24c732
Luis Cielak <luis@luiscielak.com>
2013-04-25 20:14:06 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<head>
+    <title>Empathy Totems</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h3>How do you feel?</h3>
+
+    <form method="post" action="insert.php">
+        <div class="input"><input type="submit" name="mood_sym" value=": D"></div> <br /> <br />
+        <input type="submit" name="mood_sym" value=": )"> <br /> <br />
+        <input type="submit" name="mood_sym" value=": ]"> <br /> <br />
+        <input type="submit" name="mood_sym" value=": |"> <br /> <br />
+        <input type="submit" name="mood_sym" value=": ["> <br /> <br />
+        <input type="submit" name="mood_sym" value=": ("> <br /> <br />
+        <input type="submit" name="mood_sym" value=":'("> <br /> <br />
+    </form>
+
+</body>
+</html>

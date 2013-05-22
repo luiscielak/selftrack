fb0e3c395ead277a763f60f185d89840d6551df5
Luis Cielak <luis.cielak@gmail.com>
2013-05-22 17:55:24 -0400

    add tracking web-app

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <!doctype html>
 <head>
-    <title>Empathy Totems</title>
+    <title>How do you feel?</title>
+    <meta name="viewport" content="width=device-width, initial-scale=0.5, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="no">
     <link href="css/style.css" rel="stylesheet">
 </head>
 
-<body>
-    <h3>How do you feel?</h3>
+<!-- Disable all touch-move events -->
+<script type="text/javascript">
+    function blockMove() {
+       event.preventDefault() ;
+}
+</script>
 
-    <form method="post" action="insert.php">
-        <div class="input"><input type="submit" name="mood_sym" value=": D"></div> <br /> <br />
-        <input type="submit" name="mood_sym" value=": )"> <br /> <br />
-        <input type="submit" name="mood_sym" value=": ]"> <br /> <br />
-        <input type="submit" name="mood_sym" value=": |"> <br /> <br />
-        <input type="submit" name="mood_sym" value=": ["> <br /> <br />
-        <input type="submit" name="mood_sym" value=": ("> <br /> <br />
-        <input type="submit" name="mood_sym" value=":'("> <br /> <br />
-    </form>
+
+<body ontouchmove="blockMove()">
+
+    <div class="nav">
+        <h3>Emotion + Mood</h3>
+    </div>
+    <div class="wrapper">
+
+        <div class="intro">
+            <h3><a href="emo.php"> enter emotion data &rarr;</a></h3>
+        </div>
+
+    </div>
 
 </body>
 </html>
